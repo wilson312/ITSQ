351c58dcf5176d84041aafc4214200fef02ad9ed
wilson312 <aabbaatt@gmail.com>
2017-04-25 13:38:28 +0900

    First Commit
    First Commit

---
+++ Assets/Scripts/EnemyUnits/EnemyRange.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRange : MonoBehaviour {
+    public GameObject bullet;
+    List<GameObject> target;
+	// Use this for initialization
+	void Start () {
+        target = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(target.Count != 0)
+        {
+            List<float> unitDistance = new List<float>();
+            foreach(GameObject soldier in target)
+            {
+              unitDistance.Add(Vector3.Distance(gameObject.transform.position, soldier.transform.position));
+            }
+        }
+	}
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "PlayerUnit")
+        target.Add(col.gameObject);
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "PlayerUnit")
+            target.Remove(col.gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerUnits/MazeNavigation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MazeNavigation : MonoBehaviour {
+    public Transform goal;
+    NavMeshAgent agent;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    void Update()
+    {
+        agent.destination = goal.position;
+    }
+}
